8f4f841bf7e73445ca926ef68c74e349ed0c4e05
Inzamam Malik <malikasinger1@gmail.com>
2016-12-20 21:33:46 +0500

    signup api is done

--- versions/v1/index.js
+++ versions/v1/index.js
@@ -11,24 +11,25 @@ var v1 = express.Router()
 
 v1.use(bodyParser.json());
 
-
+///////////////////////do signup started/////////////////////////////////////////////////////////////////
 v1.post("/signup", function (req, res, next) {
 
-    objectFace = {
-        email: string,
-        password: string,
-        firstName: string,      //this is a interface which is must required from front end
-        lastName: string,
-        companyName: string
-    }
+    console.log("body: ", req.body);
     var signupObject = req.body;
-    console.log("data is : ", signupObject);
-    doSignup(signupObject).then(function (success) {
-        console.log("signup success: ", success);
-        res.json({ signup: true });
-    }, function (err) {
-        console.log("signup error: ", err);
-        res.json({ signup: false, message: err });
+
+    var newUser = new userModel(signupObject);
+    newUser.save(function (err, saved) {
+        if (!err) {
+            console.log("signup success: ", saved);
+            res.json({ signup: true });
+        } else {
+            console.log("signup error: ", err);
+            res.json({
+                signup: false,
+                message: "unknown database error check lohs for detail",
+                error: err
+            });
+        }
     });
 });
 /////////////////signup request//////////////////////////////////////////////////////////////////////
